b8d41450258a1ba76c19dbfd7c28a2cfd73454a3
AdityaLok <aditya.lokhande@globallogic.com>
2019-02-21 19:55:34 +0530

    Initial task complete

---
+++ Calculator/app/src/main/java/com/example/calculator/PagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.calculator;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+int noTabs;
+
+    public PagerAdapter(FragmentManager fm, Integer noTabs) {
+        super(fm);
+        this.noTabs= noTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+         switch (position)
+         {
+             case 0:
+                 GeneralCalculator tab1= new GeneralCalculator();
+                 return tab1;
+
+             case 1:
+                 ScientificCalculator tab2=new ScientificCalculator();
+                 return tab2;
+
+             default:
+                     return null;
+         }
+    }
+
+    @Override
+    public int getCount() {
+        return noTabs ;
+    }
+}
---
+++ Calculator/app/src/main/java/com/example/calculator/ScientificCalculator.java
@@ -0,0 +1,108 @@
+package com.example.calculator;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class ScientificCalculator extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ScientificCalculator() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.scientific_operations, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    private EditText mInput_1EditText;
+    private TextView mResultTextView;
+    private String result;
+    private Double input;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mInput_1EditText = (EditText) view.findViewById(R.id.input);
+        mResultTextView = (TextView) view.findViewById(R.id.ResultTextView);
+        // button initialisation
+        Button buttonLog = (Button) view.findViewById(R.id.log);
+        Button buttonLn = (Button) view.findViewById(R.id.ln);
+        // set click listeners
+        buttonLog.setOnClickListener(onClickListener);
+        buttonLn.setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mInput_1EditText.getText().length()>0){
+                switch (v.getId()) {
+                    case R.id.log:
+                        Log.d("TAG", mInput_1EditText.getText().toString());
+                        //TODO use editTest Text
+                        input = Double.parseDouble(mInput_1EditText.getText().toString());
+                        result = String.valueOf(Math.log(input));
+                        mResultTextView.setText(result);
+                        break;
+
+                    case R.id.ln:
+                        input = Double.parseDouble(mInput_1EditText.getText().toString());
+                        result = String.valueOf(Math.pow(input,10));
+                        mResultTextView.setText(result);
+                        break;
+
+                }
+
+            }
+        }
+    };
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
